d24014257ce4738c6ceadbd74106c5bb7578c261
ThibertChristophe <c.thibert89@gmail.com>
2024-02-26 22:50:29 +0100

    First commit

---
+++ src/main/java/com/example/demo2/controller/AccountController.java
@@ -0,0 +1,16 @@
+
+package com.example.demo2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("accounts")
+class AccountController {
+
+  @GetMapping
+  public String getString() {
+    return "ALL ACCOUNT";
+  }
+}
