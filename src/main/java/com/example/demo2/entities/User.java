3176343ce97e62c5a334164240e804750b9f02a0
ThibertChristophe <c.thibert89@gmail.com>
2024-02-28 23:56:46 +0100

    Ajout de l'entité User et de son controller.

---
+++ src/main/java/com/example/demo2/entities/User.java
@@ -0,0 +1,36 @@
+package com.example.demo2.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "USER")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
+  private String email;
+
+  public User() {
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+}
