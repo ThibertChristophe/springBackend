7c324774efc83ffbcbf9ef0b2b059c00ff18b6a6
Christophe Thibert <c.thibert89@gmail.com>
2024-03-06 21:49:11 +0100

    Retrait du controller dans Main & retrait du allowedHeaders
    Le allowedHeaders pour les CORS faisait planter les requests

--- src/main/java/com/example/demo2/WebConfig.java
+++ src/main/java/com/example/demo2/WebConfig.java
@@ -13,9 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/**")
-        .allowedOrigins("http://localhost:4200")
         .allowedMethods("GET", "POST", "PUT", "DELETE")
-        .allowedHeaders("");
-
+        .allowedOrigins("http://localhost:4200");
   }
 }
