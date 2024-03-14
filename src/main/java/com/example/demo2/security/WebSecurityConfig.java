f9503f49821e4aa8691f2944f80b06ea51baa2b6
Christophe Thibert <c.thibert89@gmail.com>
2024-03-15 00:41:10 +0100

    Update WebSecurityConfig.java
    Apres MAJ de gradle etc avec IntelliJ en sudo.
    L'erreur de build est resolue

--- src/main/java/com/example/demo2/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo2/security/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.demo2.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
 }
