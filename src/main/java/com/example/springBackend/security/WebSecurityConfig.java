5a88f428cbbbb61a608021cfbddbeeeff3f0ab21
Christophe Thibert <c.thibert89@gmail.com>
2024-03-17 23:15:15 +0100

    Ajout smtp & syntaxe definition de route

--- src/main/java/com/example/springBackend/security/WebSecurityConfig.java
+++ src/main/java/com/example/springBackend/security/WebSecurityConfig.java
@@ -17,8 +17,14 @@ public class WebSecurityConfig {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable) // désactive le cross side origin CORS
                 .authorizeHttpRequests(
-                        authorize -> authorize.requestMatchers("/**").permitAll()
-                        //authorize -> authorize.requestMatchers("/home").permitAll().anyRequest().authenticated() /// autorise la route /home
+                        //authorize -> authorize.requestMatchers("/**").permitAll()
+                        authorize ->
+                                authorize
+                                        .requestMatchers("/home/*").permitAll()
+                                        .requestMatchers("/user/*").permitAll()
+                                        .requestMatchers("/auth/*").permitAll()
+                                        .requestMatchers("/booking/*").permitAll()
+                                        .anyRequest().authenticated() /// autorise la route /home
                 ).build();
     }
 
